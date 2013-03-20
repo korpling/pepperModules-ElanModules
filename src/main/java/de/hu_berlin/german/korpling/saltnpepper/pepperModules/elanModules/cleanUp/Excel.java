91997899a9f148ae5137e721e2ebe620270f0d37
ruettet <tom.ruette@hu-berlin.de>
2013-03-20 14:32:34 +0000

    eine excel darstellung von elan dateien, für die korrektur im ddd
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/ElanModules/trunk@7491 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/elanModules/cleanUp/Excel.java
@@ -0,0 +1,119 @@
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.elanModules.cleanUp;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
+
+import mpi.eudico.server.corpora.clom.Tier;
+import mpi.eudico.server.corpora.clomimpl.abstr.AbstractAnnotation;
+import mpi.eudico.server.corpora.clomimpl.abstr.TierImpl;
+import mpi.eudico.server.corpora.clomimpl.abstr.TranscriptionImpl;
+
+public class Excel {
+	
+	public static void main(String[] args) throws Exception {
+		// this is the map in which everything is stored
+		Map<Long, Map> m = new HashMap<Long, Map>();
+		
+		// get the file to work on
+		String fname = args[0];
+		String fnameout = fname.replace(".eaf", ".csv");
+		
+		// parse the Elan file
+		TranscriptionImpl eaf = new TranscriptionImpl(fname);
+		System.out.println("working on " + fname);
+		
+		// Tiernames that are interesting
+		ArrayList<String> tiernames = new ArrayList<String>();
+		tiernames.add("M1a DDDTS Lemma");
+		tiernames.add("Lemma");
+		tiernames.add("Referenztext W");
+		tiernames.add("Übersetzung");
+		tiernames.add("M1b DDDTS Beleg");
+		tiernames.add("M2a Flexion Lemma");
+		tiernames.add("M2b Flexion Beleg 1");
+		tiernames.add("M2b Flexion Beleg 2");
+		
+		// go through tiers
+		Vector<Tier> tiers = eaf.getTiers();
+		for (Tier tier : tiers){
+			TierImpl tierimpl = (TierImpl) tier;
+			if (tiernames.contains(tierimpl.getName())){
+				// go through annotations on tier
+				Vector<AbstractAnnotation> annos = tierimpl.getAnnotations();
+				for (AbstractAnnotation anno : annos){
+					long begin = anno.getBeginTimeBoundary();
+					String value = anno.getValue().trim();
+					String attr = tierimpl.getName().trim();
+					if (m.containsKey(begin)){
+						Map<String, String> mm = m.get(begin);
+						mm.put(attr, value);
+					}
+					if (!m.containsKey(begin)){
+						Map<String, String> mm = new HashMap<String, String>();
+						mm.put(attr, value);
+						m.put(begin, mm);
+					}
+				}
+			}
+		}
+		
+		StringBuffer sb = new StringBuffer();		
+		Collections.sort(tiernames);
+		sb.append("zeit;" + join( tiernames, ";") + "\n" );
+		SortedMap<Long, Map> sortedMap = new TreeMap<Long, Map>(m);
+		for (long timeslot : sortedMap.keySet()){
+			sb.append(Milliseconds2HumanReadable(timeslot));
+			for (String ebene : tiernames){
+				if (m.get(timeslot).containsKey(ebene)){
+					sb.append(";" + m.get(timeslot).get(ebene));
+				}
+				if (!m.get(timeslot).containsKey(ebene)){
+					sb.append(";");
+				}
+			}
+			sb.append("\n");
+		}
+		
+		try {
+			BufferedWriter out = new BufferedWriter(
+					new FileWriter(fnameout));
+			String outText = sb.toString();
+			out.write(outText);
+			out.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String Milliseconds2HumanReadable(long millis){
+		return String.format("%d min, %d sec", 
+			    TimeUnit.MILLISECONDS.toMinutes(millis),
+			    TimeUnit.MILLISECONDS.toSeconds(millis) - 
+			    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
+			);
+	}
+
+	public static String join(ArrayList<String> r,String d)
+	{
+	        if (r.size() == 0) return "";
+	        StringBuilder sb = new StringBuilder();
+	        int i;
+	        for(i=0;i<r.size()-1;i++)
+	            sb.append(r.get(i)+d);
+	        return sb.toString()+r.get(i);
+	}
+
+}
